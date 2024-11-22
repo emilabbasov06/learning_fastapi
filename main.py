6a0454e36968ba6a7fbc94dfabb18fd2b01c5507
emilabbasov06 <emilabb13@gmail.com>
2024-11-22 22:26:40 +0400

    learninggggg

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+  title: str
+  content: str
+  published: bool = True
+  rating: Optional[int] = None
+
+
+my_posts = [
+  {
+    "title": "Best programming languges to learn in 2025",
+    "content": "I don't know :D",
+    "id": 1
+  },
+  {
+    "title": "Favourite foods",
+    "content": "I like pizza",
+    "id": 2,
+  }
+]
 
 @app.get("/")
 def root():
   return {"message": "Hellooooo!"}
 
 
-@app.get("/")
+@app.get("/posts")
 def get_posts():
-  return {"data" : "This is your posts"}
+  return {"posts": my_posts}
+
+
+@app.post("/posts") 
+def create_post(post: Post):
+  post_dict = post.dict()
+  post_dict["id"] = randrange(0, 99999999)
+  
+  my_posts.append(post_dict)
+  return {"data": post_dict}
